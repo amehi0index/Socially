da20d640d693e44b3bdda323bb4dc7e4b2d8ccdc
Amelia Hill <amehi4yus@gmail.com>
2023-10-03 22:30:44 -0700

    Update config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
 
-import { Amplify } from 'aws-amplify';
-import config from './aws-exports';
+import { Amplify } from 'aws-amplify'
+import awsConfig from './aws-exports'
 // Amplify.configure(config);
 
 const isLocalhost = Boolean(
@@ -39,16 +39,10 @@ const updatedAwsConfig = {
 
 Amplify.configure(updatedAwsConfig);
 
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
